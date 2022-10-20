a4b8914a632e592d33c9377dcbfc897d8034f817
thales-bpl <thales.bplima@gmail.com>
2022-10-19 21:33:24 -0300

    fix: scrape_next_page_link() selector

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,7 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
-    next_page_url = selector.css(".next a::attr(href)").get()
+    next_page_url = selector.css(".next::attr(href)").get()
     return next_page_url if next_page_url else None
 
 
